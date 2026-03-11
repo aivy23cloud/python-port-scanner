51ae3a578c6b69d19bee12aa8c91be96d40d2c61
Ashton Ivy <ashtonivy23@gmail.com>
2026-03-10 23:30:11 -0500

    Added much needed safety and security features, command line argument support, more detailed output, improved error handling, and basic documentation

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# python-port-scanner
+# Python Port Scanner
+
+A lightweight multithreaded port scanner written in Python that detects open ports on a target host. This project was made to gain a better understanding of core networking concepts and techniques such as sockets, port scanning, and multithreaded execution.
+
+## Features
+
+- Scan a range of TCP ports on a target IP address
+- Detect open ports using Python sockets
+- Multithreaded execution for quicker execution time
+- Clean output and result display
+
+## Example Output:
+
+> Enter the target IP address: 192.168.1.1 <br>
+> Enter the lowest port to scan (Default: 1): 1 <br>
+> Enter the highest port to scan (Default: 1024): 1024 <br>
+>
+> Scanning for open ports [1 - 1024] on "192.168.1.1"...
+> 
+> Open Ports: <br>
+> Port 53 OPEN
+>
+> Scan completed in 11.09 seconds...
+
+## Installation Instructions:
+
+Clone the repository: <br>
+`git clone https://github.com/aivy23cloud/python-port-scanner.git`
+
+Navigate to the project folder: <br>
+`cd python-port-scanner`
+
+#### Requires Python 3.8+
+
+## Usage
+
+### Executing with command line arguments
+
+#### Execute the `main.py` file as so: <br>
+`python main.py TARGET_IP PORT_RANGE_LOW PORT_RANGE_HIGH`
+
+<br>
+
+### Executing without command line arguments
+
+#### Execute the `main.py` file: <br>
+`python main.py` <br>
+Answer prompted questions unless a default is stated
+
+## Learning Objectives
+
+This project was built to strengthen my understanding of:
+
+- Basic python syntax and usage
+- Utilization of multithreading for improved performance
+- Basic networking concepts such as sockets and ports
+- Secure input validation and error handling
+
+## Future Improvements
+
+- Implement banner grabbing
+- Quicker execution time
+
+
+## Disclaimer
+
+This tool is strictly intended for learning purposes on systems you are permitted to test or own.
+
+![Python](https://img.shields.io/badge/python-3.13-blue)
+![License](https://img.shields.io/badge/license-MIT-green)
--- main.py
+++ main.py
@@ -1,46 +1,114 @@
+import sys as Sys
 import socket as Socket
+import ipaddress
+import time
 
 from threading import Thread, Lock
+from queue import Empty, Queue
+
+#Basic Config Settings
+MAX_THREADS = 100
+
+DEFAULT_PORT_RANGE_LOW = 1
+DEFAULT_PORT_RANGE_HIGH = 1024
 
 open_ports = []
-closed_ports = []
-
 lock = Lock()
+port_queue = Queue()
 
-def scan_port(ip, port):
+def scan_tcp_port(ip, port):
     try:
-        sock = Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM)
-        sock.settimeout(1)
+        with Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            result = sock.connect_ex((ip, port))
         
-        result = sock.connect_ex((ip, port))
-        sock.close()
-        
-        with lock:
-            if (result == 0):
+        if (result == 0):
+            with lock:
                 open_ports.append(port)
-            else:
-                closed_ports.append(port)
-    except:
-        with lock:
-            closed_ports.append(port)
+    except Exception:
+        exit()
+        return
     
-def display_port_lists():
+def print_open_ports():
+    if (len(open_ports) == 0):
+        print("No open ports found.")
+        return
+    
     print("Open Ports:")
-    for port in open_ports:
-        print(port)
+    
+    for port in sorted(open_ports):
+        print(f"Port {port} OPEN")
+        
+def get_scan_config():
+    try:
+        ip = Sys.argv[1]
+    except IndexError:
+        ip = input("Enter the target IP address: ")
+        
+    try:
+       ipaddress.ip_address(ip)
+    except ValueError:
+        print("Invalid IP Address")
+        exit()
+        
+    try:
+        port_range_low = int(Sys.argv[2])
+    except IndexError:
+        raw = input(f"Enter the lowest port to scan (Default: {DEFAULT_PORT_RANGE_LOW}): ").strip()
+        port_range_low = int(raw) if raw else DEFAULT_PORT_RANGE_LOW
+    except ValueError:
+        port_range_low = DEFAULT_PORT_RANGE_LOW
+        
+    try:
+        port_range_high = int(Sys.argv[3])
+    except IndexError:
+        raw = input(f"Enter the lowest port to scan (Default: {DEFAULT_PORT_RANGE_HIGH}): ").strip()
+        port_range_high = int(raw) if raw else DEFAULT_PORT_RANGE_HIGH
+    except ValueError:
+        port_range_high = DEFAULT_PORT_RANGE_HIGH
+        
+    if (port_range_low > port_range_high):
+        print("Invalid Port Range: High must be greater than low")
+        exit()
+        
+    if (port_range_low < 0):
+        print("Invalid Port Range: Low must be greater than 0")
+        exit()
+        
+    if (port_range_high > 65535):
+        print("Invalid Port Range: High must be lower than 65,535")
+        exit()
+        
+    return ip, range(port_range_low, port_range_high + 1)
 
-target_ip = input("Enter the target IP address: ")
-port_range = range(1, 1025)
+def worker(ip):
+    while True:
+        try:
+            port = port_queue.get_nowait()
+        except Empty:
+            break
+        
+        scan_tcp_port(ip, port)
+        port_queue.task_done()
+
+target_ip, port_range = get_scan_config()
+
+for port in port_range:
+    port_queue.put(port)
+
+print(f'\nScanning for open ports [{port_range.start} - {port_range.stop - 1}] on "{target_ip}"...\n')
+start = time.time()
 
 threads = []
-for port in port_range:
-    thread = Thread(target=scan_port, args=(target_ip, port))
+for _ in range(MAX_THREADS):
+    thread = Thread(target=worker, args=(target_ip,))
+    thread.start()
     threads.append(thread)
     
 for thread in threads:
-    thread.start()
-    
-for thread in threads:
     thread.join()
+    
+port_queue.join()
 
-display_port_lists()
+print_open_ports()
+print(f"\nScan completed in {time.time() - start:.2f} seconds...")
